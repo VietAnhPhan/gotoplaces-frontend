56a7a21584dfbb79dce7888ce78c3977f14fe668
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-11-26 22:31:42 +0700

    Remove unnecessary brackets

--- src/components/HydrationLoader.jsx
+++ src/components/HydrationLoader.jsx
@@ -3,11 +3,9 @@ import styles from "./HydrationLoader.module.css";
 export default function HydrationLoader() {
   return (
     <>
-      (
       <div className={styles.loaderOverlay}>
         <div className={styles.loader}></div>
       </div>
-      )
     </>
   );
 }
